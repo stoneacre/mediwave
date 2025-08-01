24592f67185e14c36698cac92da3f87460076117
Charlie Astrada <carlosastrada@gmail.com>
2025-08-01 18:04:06 -0300

    chore: add commitlint with simple conventional config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,18 +1,3 @@
-/** @type {import('@commitlint/types').UserConfig} */
 export default {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',    // new features/sections
-        'fix',     // bug fixes
-        'content', // MDX/content changes
-        'style',   // visual/CSS only changes
-        'docs',    // documentation changes
-        'chore'    // dependency updates, etc
-      ],
-    ],
-  },
+  extends: ['@commitlint/config-conventional']
 };
---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import eslintPluginAstro from 'eslint-plugin-astro';
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    files: ['public/**/*.js'],
+    languageOptions: {
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        setTimeout: 'readonly',
+        setInterval: 'readonly',
+        clearInterval: 'readonly',
+        IntersectionObserver: 'readonly'
+      }
+    }
+  },
+  {
+    files: ['**/*.config.{js,ts}', '.eslintrc.cjs'],
+    languageOptions: {
+      globals: {
+        module: 'readonly',
+        require: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly'
+      }
+    }
+  }
+];
