e1a92bccb3ae36bd8adddde0c6114e26bbc759a7
Charlie Astrada <carlosastrada@gmail.com>
2025-08-01 21:27:07 -0300

    chore: remove TinaCMS generated files from git tracking

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/carlos/Sites/meteor-affinity/mediwave/tina/__generated__/.cache/1754091241227', url: 'https://content.tinajs.io/1.6/content/632e9e84-ff71-4f1e-9fe9-5b697914b875/github/main', token: '85e8a61ed1fdf19af65d46eea931997ffb2d9d40', queries,  });
-export default client;
-  
